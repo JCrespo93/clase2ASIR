96d7e9d9b176e8f7cdc576cf644298b0da8db3f9
Jesús <jcresporuiz@hotmail.com>
2021-11-08 10:23:00 +0100

    commit 08/11/21

--- array.php
+++ array.php
@@ -1,48 +1,148 @@
 <?php
 
 
-#ARRAY NORMALES
+//Bucles anidados de forma independiente
+print "<br><b>BUCLE INDEPENDENTES</b>"."<br>";
+for ($inicio=1; $inicio < 5 ; $inicio++) { //Bucle exterior
 
-$numeros = array(8, 3, 7, 1);
+    print "<b>Vamos por la iteracción (bucle exterior): ".$inicio."</b><br>";
 
-$suspensos = array("Migue","Kerry");
+    for ($inicio2=0; $inicio2 < 5 ; $inicio2++) {  //Bucle interior
 
-$coches= array("Ferrari","Seat","BMW","Opel","Mercedes");
-
-
-echo $suspensos[0];
-echo $numeros[3];
-echo $coches[1];
-
-
-
-$cuentaCoches= count($coches);
-
-
-echo "Número de coches".$cuentaCoches."<br>";
-
-$aleatorio=rand(0,4);
-
-
-echo "Coche aleatorio:".$coches[$aleatorio]."<br>";
-
-$parada=0;
-
-while ($parada < 5) {
-    
-    $aleatorio=rand(0,4);
-    echo $coches[$aleatorio]."<br>";
-    $parada++;
-    
-
+        
+        print "Vamos por la iteracción inicio2 (bucle interior): ".$inicio2."<br>";
+    }
 }
-#ARRAY ASOCIATIVOS
 
-$alumno = array("Nombre" => "Migue",
-                "Apellidos"=>"Kerry",
-                "Edad" =>23,
-                "estaAprobado"=>false);
+//Bucles anidados de forma dependiente
 
-echo"El alumno es: " .$alumno["Nombre"].$alumno["Apellidos"].$alumno["Edad"];
+print "<br><b>BUCLE DEPENDIENTE</b>"."<br>";
+for ($varexterior=0; $varexterior < 5 ; $varexterior++) { 
+    
+    print "<b>Muestro variable exterior: ".$varexterior."</b><br>";
+
+    for ($varinterior=0; $varinterior < $varexterior ; $varinterior++) { 
+
+        print "Muestro variable interior: ".$varinterior."<br>";
+
+    }
+}
+
+
+
+#ARRAYS
+
+print "<br><h3>ARRAY 'ESTÁNDAR'</h3>"."<br>";
+
+
+#Forma 1:
+
+$numerosP = array(2,4,6,8,10);
+
+print_r($numerosP);
+echo "<br>";
+print "Muestro lo que hay en la posición 0: ".$numerosP[0]."<br>";
+
+$cuentaNumerosP=count($numerosP);
+print "Valor de Cuenta Array: ".$cuentaNumerosP."<br><br>";
+
+
+for ($i=0; $i < $cuentaNumerosP; $i++) { 
+    print "Muestro lo que hay en la posición ".$i." : ".$numerosP[$i]."<br>";
+}
+echo "<br>";
+#Forma 2:
+
+$numerosI = [1,3,5,7,9];
+
+print_r($numerosI);
+echo "<br>";
+
+
+#ARRAYS ASOCIATIVOS
+
+print "<br><h3>ARRAY ASOCIATIVOS</h3>"."<br>";
+
+$potencias2=[ 1=>2 , 2=>4 ,3 =>8];
+$capitales= array("Andalucia"=> "Sevilla", 
+"Madrid" => "Madrid",
+ "Aragón"=>"Zaragoza",
+  "Baleares" =>"Palma de Mallorca");
+
+print_r($potencias2);
+echo "<br>";
+print_r($capitales);
+echo "<br>";
+
+//Añadir elementos a un array
+
+$capitales["Cataluña"]="Barcelona";
+
+print_r($capitales);
+echo "<br>";
+
+$capitales["Andalucia"]="Almeria";
+
+print_r($capitales);
+echo "<br>";
+
+$cuentaLetras=strlen($capitales["Andalucia"]);
+echo $cuentaLetras;
+
+$notasAsignaturasMigue=["LMI"=>0,
+"FOL"=>10,
+"PAR"=>9,
+"ISO"=>0,
+"FH"=>0,
+"BD"=>4];
+
+print_r($notasAsignaturasMigue);
+
+//Unión de Arrays
+
+echo "<br>";
+
+$coches1= array("Seat","Renault","Dacia");
+$coches2= ["BMW","Audi"];
+
+print_r($coches1);
+echo "<br>";
+print_r($coches2);
+echo "<br> Unión de arrays <br>";
+
+$union1con2=$coches1+$coches2;
+print_r($union1con2);
+echo "<br>";
+
+$union2con1=$coches2+$coches1;
+print_r($union2con1);
+echo "<br>";
+
+
+//Borrar elemento
+
+$coches3= array("Seat","Renault","Dacia","BMW","Audi");
+print_r($coches3);
+echo "<br>";
+unset($coches3[5]);
+
+
+print_r($coches3);
+echo "<br>";
+$cuentaCoches=count($coches3);
+echo $cuentaCoches;
+
+print $coches3[]="Ferrari";
+print_r($coches3);
+echo "<br>";
+
+
+//Copiar matrices
+
+$copiaCapitales = $capitales;
+$copiaCapitales["Andalucia"]="Córdoba";
+print_r($copiaCapitales);
+echo "<br>";
+
 
 ?>
